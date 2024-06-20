ced2449546ff541ad4e8914fc58849aa448f673b
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-06-21 01:49:27 +0800

    adding conversion to rnx

--- data_downloader/uploading_files.py
+++ data_downloader/uploading_files.py
@@ -5,11 +5,12 @@ import zipfile
 import gzip
 import os
 import logging
-
+import subprocess
+import shutil
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("DataDownloader")
 
-date = (datetime.now() - timedelta(days=3)).strftime("%Y-%m-%d")
+date = (datetime.now() - timedelta(days=5)).strftime("%Y-%m-%d")
 
 link = f"https://api.simurg.space/datafiles/map_files?date={date}"
 file_name = f"{date}.zip"
@@ -20,7 +21,7 @@ def download_file(url, file_name):
         response = requests.get(url, stream=True)
         total_length = response.headers.get('content-length')
 
-        if total_length is None:  # отсутствует заголовок content-length
+        if total_length is None:
             f.write(response.content)
         else:
             dl = 0
@@ -33,12 +34,14 @@ def download_file(url, file_name):
                 sys.stdout.flush()
     logger.info("\nСкачивание завершено")
 
+
 def unzip_file(zip_file, extract_to):
     with zipfile.ZipFile(zip_file, 'r') as zip_ref:
         zip_ref.extractall(extract_to)
     logger.info(f"Распаковано {zip_file} в {extract_to}/")
     os.remove(zip_file)
     logger.info(f"Удален {zip_file}")
+
 
 def decompress_gz_files(directory):
     for root, dirs, files in os.walk(directory):
@@ -53,13 +56,34 @@ def decompress_gz_files(directory):
                 os.remove(gz_file_path)
                 logger.info(f"Удален {gz_file_path}")
 
+
+def convert_crx_to_rnx(directory):
+    crx2rnx_path = "CRX2RNX" 
+    if not shutil.which(crx2rnx_path):
+        logger.error(f"Команда {crx2rnx_path} не найдена. Убедитесь, что инструмент установлен и доступен в PATH.")
+        sys.exit(1)
+
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".crx"):
+                crx_file_path = os.path.join(root, file)
+                rnx_file_path = os.path.join(root, file.replace(".crx", ".rnx"))
+                command = f"{crx2rnx_path} {crx_file_path} -f -d"
+                try:
+                    subprocess.run(command, check=True, shell=True)
+                    logger.info(f"Конвертирован {crx_file_path} в {rnx_file_path}")
+                except subprocess.CalledProcessError as e:
+                    logger.error(f"Ошибка конвертации {crx_file_path}: {e}")
+                else:
+                    if os.path.exists(crx_file_path):
+                        os.remove(crx_file_path)
+                        logger.info(f"Удален {crx_file_path} после конвертации в {rnx_file_path}")
+
+
 if __name__ == "__main__":
     download_file(link, file_name)
-
-    # Распаковка zip-файла
     unzip_file(file_name, date)
-
-    # Декомпрессия файлов .gz (включая повторную проверку в разархивированных папках)
     decompress_gz_files(date)
+    convert_crx_to_rnx(date)
 
-    logger.info("Все файлы успешно загружены и декомпрессированы")
+    logger.info("Все файлы успешно загружены, декомпрессированы и конвертированы")
