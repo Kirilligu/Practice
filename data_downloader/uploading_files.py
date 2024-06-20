797505c018f0c37f1e68463e82c82d82a003d603
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-06-20 18:21:01 +0800

    Added deletion of the original zip file
    The deletion of the original zip file has been added, logging has also been added and now in the unzipped file.gz files are being decompressed

--- data_downloader/uploading_files.py
+++ data_downloader/uploading_files.py
@@ -4,43 +4,62 @@ from datetime import datetime, timedelta
 import zipfile
 import gzip
 import os
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("DataDownloader")
 
 date = (datetime.now() - timedelta(days=3)).strftime("%Y-%m-%d")
 
 link = f"https://api.simurg.space/datafiles/map_files?date={date}"
 file_name = f"{date}.zip"
 
-with open(file_name, "wb") as f:
-    print(f"Скачивание {file_name}")
-    response = requests.get(link, stream=True)
-    total_length = response.headers.get('content-length')
+def download_file(url, file_name):
+    with open(file_name, "wb") as f:
+        logger.info(f"Скачивание {file_name}")
+        response = requests.get(url, stream=True)
+        total_length = response.headers.get('content-length')
 
-    if total_length is None: 
-        f.write(response.content)
-    else:
-        dl = 0
-        total_length = int(total_length)
-        for data in response.iter_content(chunk_size=4096):
-            dl += len(data)
-            f.write(data)
-            done = int(50 * dl / total_length)
-            sys.stdout.write(f"\r[{'=' * done}{' ' * (50 - done)}]")
-            sys.stdout.flush()
-print("\nСкачивание завершено")
+        if total_length is None:  # отсутствует заголовок content-length
+            f.write(response.content)
+        else:
+            dl = 0
+            total_length = int(total_length)
+            for data in response.iter_content(chunk_size=4096):
+                dl += len(data)
+                f.write(data)
+                done = int(50 * dl / total_length)
+                sys.stdout.write(f"\r[{'=' * done}{' ' * (50 - done)}]")
+                sys.stdout.flush()
+    logger.info("\nСкачивание завершено")
 
-# Распаковка zip-файла
-with zipfile.ZipFile(file_name, 'r') as zip_ref:
-    zip_ref.extractall(date)
-print(f"Распаковано {file_name} в {date}/")
+def unzip_file(zip_file, extract_to):
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+    logger.info(f"Распаковано {zip_file} в {extract_to}/")
+    os.remove(zip_file)
+    logger.info(f"Удален {zip_file}")
 
-# Декомпрессия файлов .gz
-for root, dirs, files in os.walk(date):
-    for file in files:
-        if file.endswith(".gz"):
-            gz_file_path = os.path.join(root, file)
-            output_file_path = os.path.join(root, file[:-3])    # Удаление расширения .gz
-            with gzip.open(gz_file_path, 'rb') as gz_file:
-                with open(output_file_path, 'wb') as out_file:
-                    out_file.write(gz_file.read())
+def decompress_gz_files(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".gz"):
+                gz_file_path = os.path.join(root, file)
+                output_file_path = os.path.join(root, file[:-3])  # Удаление расширения .gz
+                with gzip.open(gz_file_path, 'rb') as gz_file:
+                    with open(output_file_path, 'wb') as out_file:
+                        out_file.write(gz_file.read())
+                logger.info(f"Декомпрессирован {gz_file_path} в {output_file_path}")
+                os.remove(gz_file_path)
+                logger.info(f"Удален {gz_file_path}")
 
-print("Декомпрессия завершена")
+if __name__ == "__main__":
+    download_file(link, file_name)
+
+    # Распаковка zip-файла
+    unzip_file(file_name, date)
+
+    # Декомпрессия файлов .gz (включая повторную проверку в разархивированных папках)
+    decompress_gz_files(date)
+
+    logger.info("Все файлы успешно загружены и декомпрессированы")
